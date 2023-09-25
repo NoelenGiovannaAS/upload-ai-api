f39941d70a0d7027c2d0a9044bc16caa1f22b198
Noh <noelen_g@outlook.com>
2023-09-24 22:45:04 -0300

    finaliza código backend

--- src/routes/get-generate-ai-completion.ts
+++ src/routes/get-generate-ai-completion.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import { prisma } from "../lib/prisma";
 import { z } from "zod";
+import { streamToResponse, OpenAIStream } from "ai";
 import { createReadStream } from "node:fs";
 import { openai } from "../lib/openai";
 
@@ -8,11 +9,11 @@ export async function getGenerateAiCompletionRoute(app: FastifyInstance) {
   app.post("/ai/generate", async (req, reply) => {
     const bodySchema = z.object({
       videoId: z.string().uuid(),
-      template: z.string(),
+      prompt: z.string(),
       temperature: z.number().min(0).max(1).default(0.5),
     });
 
-    const { videoId, template, temperature } = bodySchema.parse(req.body);
+    const { videoId, prompt, temperature } = bodySchema.parse(req.body);
 
     const video = await prisma.video.findUniqueOrThrow({
       where: {
@@ -26,7 +27,7 @@ export async function getGenerateAiCompletionRoute(app: FastifyInstance) {
       });
     }
 
-    const promptMessage = template.replace(
+    const promptMessage = prompt.replace(
       "{transcription}",
       video.transcription
     );
@@ -35,8 +36,15 @@ export async function getGenerateAiCompletionRoute(app: FastifyInstance) {
       model: "gpt-3.5-turbo-16k",
       temperature,
       messages: [{ role: "user", content: promptMessage }],
+      stream: true,
     });
 
-    return response;
+    const stream = OpenAIStream(response);
+    streamToResponse(stream, reply.raw, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Acess-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+      },
+    });
   });
 }
